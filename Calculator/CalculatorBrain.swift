d436b30624a0aa559f68eadcd8f39a81471addc3
Denver Stove <dtstove@gmail.com>
2018-10-18 09:58:42 +1100

    Calculator brain struct files

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Denver Stove on 18/10/18.
+//  Copyright © 2018 Denver Stove. All rights reserved.
+//
+
+import Foundation
+
+
+struct CalculatorBrain {
+    
+}
