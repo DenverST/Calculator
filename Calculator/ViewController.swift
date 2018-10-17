767944d666f811f4235caffe0db7889eab355778
Denver Stove <dtstove@gmail.com>
2018-10-17 11:05:44 +1100

    Finished lecture 1, made all number buttons with pi and square root function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
     
     @IBAction func touchDigit(_ sender: UIButton) {
-        print("touchDigit was called")
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+          return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathemitcalSymbol = sender.currentTitle {
+            switch mathemitcalSymbol {
+            case "∏":
+                displayValue = Double.pi
+            case "√":
+               displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
     }
     
 }
