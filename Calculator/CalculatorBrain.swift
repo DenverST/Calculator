a19413f33da34a638de119668e2171960ce8d6a3
Denver Stove <dtstove@gmail.com>
2018-10-30 16:09:34 +1100

    Added a memory cycle to practice breaking them

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,11 @@ import Foundation
 
 struct CalculatorBrain {
     
+    mutating func addUnaryOperation(named symbol: String, _ operation: @escaping (Double) -> Double) {
+        operations[symbol] = Operation.unaryOperation(operation
+        )
+    }
+    
     private var accumulator: Double?
     
     private enum Operation {
